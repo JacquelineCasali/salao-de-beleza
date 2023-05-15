965ec31a2479b0329e789f33bb68912644154348
JacquelineCasali <jcasalia@yahoo.com.br>
2023-05-15 09:48:56 -0300

    adaptando o pagina para telas menores

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 const form = document.querySelector("#form");
 // selecionar cada imput sao valores indepedentes
 const nameInput = document.querySelector("#name");
-const telefoneInput = document.querySelector("#telefone");
-
 const emailInput = document.querySelector("#email");
-const passwordInput = document.querySelector("#password");
+const telefoneInput = document.querySelector("#telefone");
 const jobSelect = document.querySelector("#job");
-const menssageTextarea = document.querySelector("#menssage");
+
+// const menssageTextarea = document.querySelector("#menssage");
+
+// funcçao do input piscar
+// Da foco a um campo de input quando a pagina é recarregada:
+window.onload = function () {
+  document.getElementById("name").focus();
+};
 
 // função validação
 form.addEventListener("submit", (event) => {
@@ -15,37 +20,30 @@ form.addEventListener("submit", (event) => {
 
   // verifica se o nome está vazio o
   if (nameInput.value === "") {
-    alert("Por favor, preencha seu nome");
+    alert("Por favor, preencha seu nome com o sobrenome");
     return;
   }
-
-  // verifica se o nome está vazio o
-  if ((telefoneInput.value, 8)) {
-    alert("O numero de telefone precisa de no mínio de 8 dígitos");
-    return;
-  }
-
   // se o email está preenchido e se é valido
   if (emailInput.value === "" || !isEmailValid(emailInput.value)) {
     alert("Por favor, preencha seu email");
     return;
   }
-  // verificar se a senha está preenchida e com 8 caracteres
-  if (!validatePassword(passwordInput.value, 8)) {
-    alert("A senha precisa de no mínio de 8 dígitos");
+  // verifica se o nome está vazio o
+  if (!validateTelefone(telefoneInput.value, 8)) {
+    alert("O numero de telefone precisa de no mínio de 8 dígitos");
     return;
   }
 
-  // verifica se o plano  foi selecionado
   if (jobSelect.value === "") {
-    alert("Por favor, escolha um plano.");
+    alert("Por favor, escolha o turno que quer ser atendido.");
     return;
   }
+
   // verifica se a mensagem está preenchida
-  if (menssageTextarea.value === "") {
-    alert("Por favor escreva uma mensagem.");
-    return;
-  }
+  // if (menssageTextarea.value === "") {
+  //   alert("Por favor escreva uma mensagem.");
+  //   return;
+  // }
 
   //  se todos os campos estiverem corretamente preenchido envie o formulário (form)
   form.submit();
@@ -69,25 +67,4 @@ function validateTelefone(numero, minDigits) {
     return true;
   }
   return false;
-}
-
-// função que valida a senha
-function validatePassword(password, minDigits) {
-  if (password.length >= minDigits) {
-    return true;
-  }
-  return false;
-}
-
-// confere senha
-
-function confereSenha() {
-  const senha = document.querySelector("input[name=senha]");
-  const confirma = document.querySelector("input[name=confirma]");
-
-  if (confirma.value === senha.value) {
-    confirma.setCustomValidity("");
-  } else {
-    confirma.setCustomValidity("As senhas não confere");
-  }
 }
